48f3336535186663c69f845afb2211f67a963d1a
brovnie <bronka1.dev@gmail.com>
2024-09-14 15:32:37 +0200

    Setup pages + Routes

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Sidebar from './Sidebar';
+import Searchbar from './Searchbar';
+import MusicPlayer from './MusicPlayer/MusicPlayer';
+import TopPlay from './TopPlay';
+export { Sidebar, Searchbar, MusicPlayer, TopPlay };
